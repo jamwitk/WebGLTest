443b601d13a9a668cb6b6df68844768c88c22a22
Jamshid <hushdilcss@gmail.com>
2023-08-15 17:23:05 +0300

    ads

--- StreamingAssets/WebGL/map.js
+++ StreamingAssets/WebGL/map.js
@@ -3,6 +3,7 @@
 async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
     const { spherical } = await google.maps.importLibrary("geometry");
+    const {LatLngAltitude} = await google.maps.importLibrary("core")
     const myLatlng = {
         lat: 22.542378,
         lng: 88.314204
@@ -11,47 +12,9 @@ async function initMap() {
     map = new Map(document.getElementById("map"), {
         center: myLatlng,
         zoom: 17,
+        mapTypeId: "terrain",
     });
-    // const markList = [];
-    // const bermudaTriangle = new google.maps.Polygon({
-    //     paths: markList,
-    //     strokeColor: "#FF0000",
-    //     strokeOpacity: .8,
-    //     strokeWeight: 4,
-    //     fillColor: "#303030",
-    //     fillOpacity: 3,
-    //     editable: true,
-    // });
-    // map.addListener("click", (MouseEvent) => {
-    //     // get lat/lon of click
-    //
-    //     const clickLat = MouseEvent.latLng.lat();
-    //     const clickLon = MouseEvent.latLng.lng();
-    //     console.log(clickLat + " " + clickLon);
-    //     // const mark = new google.maps.Marker({
-    //     //     position: new google.maps.LatLng(clickLat, clickLon),
-    //     //     map,
-    //     //     title: "Click to zoom",
-    //     // });
-    //     var sCount = markList.push({
-    //         lat: clickLat,
-    //         lng: clickLon
-    //     });
-    //    
-    //     // Add a listener for the set_at  event.
-    //     google.maps.event.addListener(bermudaTriangle.getPath(), 'set_at', function() {
-    //         console.log("set_at");
-    //         const areas = spherical.computeArea(bermudaTriangle.getPath());
-    //         console.log(areas)
-    //     });
-    //     bermudaTriangle.setMap(map);
-    //     if (sCount >= 4) {
-    //         console.log("4 oldu hesapla");
-    //         const areas = spherical.computeArea(bermudaTriangle.getPath());
-    //         console.log(areas)
-    //     }
-    //
-    // });
+   
     poly = new google.maps.Polyline({
         strokeColor: '#00DB00',
         strokeOpacity: 1.0,
@@ -59,8 +22,8 @@ async function initMap() {
         fillColor: 'green',
         fillOpacity: 0.05
     });
+    
     poly.setMap(map);
-
     map.addListener('click', addLatLng);
 }
 var poly;
@@ -73,40 +36,45 @@ function addLatLng(event) {
         return;
     }
     if (path.length === 3) {
-        for (let i = 0; i < path.getLength(); i++) {
-            const p1 = path.getAt(i);
-            const p2 = path.getAt((i + 1) % path.getLength());
-            const distance = google.maps.geometry.spherical.computeDistanceBetween(p1, p2);
-            distances.push(distance);
-            // write biggest distance
-           
-        }
-        const max = Math.max(...distances);
-        const min = Math.min(...distances);
-        console.log(max);
-        console.log(min);
+        // enable button
+        console.log(path.getAt(0).lat()+" "+path.getAt(0).lng()+" "+path.getAt(0));
     }
     if (path.length === 2) {
         var polygonOptions = { path: path, strokeColor: "#00DB00", fillColor: "green" };
         polygon = new google.maps.Polygon(polygonOptions);
         polygon.setMap(map);
-        
     }
-
+    //how to use LatLngAltitudeLiteral to get altitude
+    //
+    
     path.push(event.latLng);
     var index = -1;
-
     function dragStartEvent(event) {
         const arr = path.getArray();
-        index = arr.indexOf(event.latLng);
-
+        index = arr.indexOf({lat: event.LatLngAltitude.lat,lng: event.LatLngAltitude.lng});
     }
     if(polygon !== undefined){
         area = google.maps.geometry.spherical.computeArea(polygon.getPath());
         console.log(area);
     }
     //find the longest line and find distance
-    
+    function createVirtualYard(){
+        for (let i = 0; i < path.getLength(); i++) {
+            const p1 = path.getAt(i);
+            const p2 = path.getAt((i + 1) % path.getLength());
+            const distance = google.maps.geometry.spherical.computeDistanceBetween(p1, p2);
+            distances.push(distance);
+            // write biggest distance
+        }
+        const max = Math.max(...distances);
+        const min = Math.min(...distances);
+        const message = area+"-"+max+"-"+min+
+            "-"+path.getAt(0).lat()+" "+path.getAt(0).lng()+
+            "-"+path.getAt(1).lat()+" "+path.getAt(1).lng()+
+            "-"+path.getAt(2).lat()+" "+path.getAt(2).lng()+
+            "-"+path.getAt(3).lat()+" "+path.getAt(3).lng()
+        unityInstance.SendMessage('VirtualYardManager','SetData',message);    
+    }
     function dragEndEvent(event) {
         if (index > -1) {
             path.setAt(index, event.latLng);
@@ -115,7 +83,7 @@ function addLatLng(event) {
         }
     }
     var marker = new google.maps.Marker({
-        position: event.latLng,
+        position: { lat: event.latLng.lat(), lng: event.latLng.lng()},
         title: '#' + path.getLength(),
         map: map,
         draggable: true,
